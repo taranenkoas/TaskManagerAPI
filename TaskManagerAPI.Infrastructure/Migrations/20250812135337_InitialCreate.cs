c592da7293017605f843d2f479003a14310addef
taranenkoas <taranenkoas97@gmail.com>
2025-08-12 17:11:30 +0300

    Add Migration

---
+++ TaskManagerAPI.Infrastructure/Migrations/20250812135337_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿namespace TaskManagerAPI.Infrastructure.Migrations;
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "TaskItems",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Status = table.Column<int>(type: "int", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                OwnerId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_TaskItems", x => x.Id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "TaskItems");
+    }
+}
