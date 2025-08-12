8525f6c8af9e6aa78d922032353633b4e5db0b89
taranenkoas <taranenkoas97@gmail.com>
2025-08-12 16:26:51 +0300

    Create TaskItem.cs

---
+++ TaskManagerAPI.Domain/Entities/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerAPI.Domain.Entities;
+
+public enum TaskStatus { New = 0, InProgress = 1, Done = 2 }
+
+internal class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public TaskStatus Status { get; set; } = TaskStatus.New;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string? OwnerId { get; set; } // позже для auth
+}
